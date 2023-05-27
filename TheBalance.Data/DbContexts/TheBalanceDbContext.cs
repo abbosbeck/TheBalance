db74a852a5172be9e9b37230859c648a0a4c9ddb
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-05-27 17:03:06 +0500

    DATA: Create DBSets

--- TheBalance.Data/DbContexts/TheBalanceDbContext.cs
+++ TheBalance.Data/DbContexts/TheBalanceDbContext.cs
@@ -4,10 +4,18 @@
 //--------------------------------------------------
 
 using Microsoft.EntityFrameworkCore;
+using TheBalance.Domain.Entities.Expenses;
+using TheBalance.Domain.Entities.Incomes;
 
 namespace TheBalance.Data.DbContexts
 {
     public class TheBalanceDbContext : DbContext
     {
+        public TheBalanceDbContext(DbContextOptions<TheBalanceDbContext> options) : base(options) { }
+
+        public virtual DbSet<Income> Incomes { get; set; }
+        public virtual DbSet<IncomeSummary> IncomeSummaries { get; set; }
+        public virtual DbSet<Expense> Expenses { get; set; }
+        public virtual DbSet<ExpenseSummary> ExpenseSummaries { get; set; }
     }
 }
