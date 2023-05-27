1d67aa72b75761b19559118bb8038f6f1726a855
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-05-27 16:46:20 +0500

    DATA: Connection Db

---
+++ TheBalance.Data/DbContexts/TheBalanceDbContext.cs
@@ -0,0 +1,13 @@
+﻿//--------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by TheBalance Team
+//--------------------------------------------------
+
+using Microsoft.EntityFrameworkCore;
+
+namespace TheBalance.Data.DbContexts
+{
+    public class TheBalanceDbContext : DbContext
+    {
+    }
+}
