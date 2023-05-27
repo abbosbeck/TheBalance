c4f123384b5552cbf93a3d6354b3543e9fac0091
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-05-27 14:49:38 +0500

    DOMAIN: Create Expense Entity

---
+++ TheBalance.Domain/Entities/Expenses/Expense.cs
@@ -0,0 +1,16 @@
+﻿//--------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by TheBalance Team
+//--------------------------------------------------
+
+using TheBalance.Domain.Commons;
+
+namespace TheBalance.Domain.Entities.Expenses
+{
+    public class Expense : Auditable
+    {
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public string Description { get; set; }
+    }
+}
