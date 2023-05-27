bac3d226bb5644339f4a1df88cae1d42f4858ea4
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-05-27 17:25:36 +0500

    DATA: Create IGenericRepository

---
+++ TheBalance.Data/IRepositories/IGeneriRepository.cs
@@ -0,0 +1,21 @@
+﻿//--------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by TheBalance Team
+//--------------------------------------------------
+
+using System.Linq.Expressions;
+
+namespace TheBalance.Data.IRepositories
+{
+    public interface IGeneriRepository<T> where T : class
+    {
+        ValueTask<T> CreateAsync(T entity);
+        T Update(T entity);
+        ValueTask<T> DeleteAsync(Expression<Func<T, bool>> expression);
+        ValueTask<T> GetAsync(Expression<Func<T, bool>> expression, string[] includes = null);
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression,
+            string[] includes = null,
+            bool isTracking = true);
+        ValueTask SaveChangesAsync();
+    }
+}
