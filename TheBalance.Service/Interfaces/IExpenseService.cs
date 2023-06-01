52e6efe07076af61f4a62628824cff206be290e7
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-06-01 18:14:40 +0500

    SERVICE: Create IExpenceService

--- TheBalance.Service/Interfaces/IExpenseService.cs
+++ TheBalance.Service/Interfaces/IExpenseService.cs
@@ -3,10 +3,17 @@
 // Developed by TheBalance Team
 //--------------------------------------------------
 
+using System.Linq.Expressions;
+using TheBalance.Domain.Entities.Expenses;
+using TheBalance.Service.DTOs.Expenses;
+
 namespace TheBalance.Service.Interfaces
 {
     public interface IExpenseService
     {
-
+        ValueTask<IEnumerable<Expense>> GetAllAsync(Expression<Func<Expense, bool>> expression = null);
+        ValueTask<Expense> CreateAsync(ExpenseForCreateDTO expense);
+        ValueTask<bool> DeleteAsync(long id);
+        ValueTask<Expense> UpdateAsync(long id, ExpenseForCreateDTO dto);
     }
 }
