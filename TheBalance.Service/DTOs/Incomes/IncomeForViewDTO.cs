332b4b591a8a52a9ddf221dd91799f11a954786d
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-06-01 15:32:41 +0500

    SERVICE: Install IncomeForViewDTO

--- TheBalance.Service/DTOs/Incomes/IncomeForViewDTO.cs
+++ TheBalance.Service/DTOs/Incomes/IncomeForViewDTO.cs
@@ -7,5 +7,9 @@ namespace TheBalance.Service.DTOs.Incomes
 {
     public class IncomeForViewDTO
     {
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public string Description { get; set; }
+        public IncomeSummaryForViewDTO IncomeSummary { get; set; }
     }
 }
